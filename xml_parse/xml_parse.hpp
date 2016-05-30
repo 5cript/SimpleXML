76d4b1f972bc15aab8d1855de7d91608b4341b84
5cript <Tim06TR@gmail.com>
2016-05-30 19:57:27 +0200

    Added combined cnvenience header for parsing.

--- xml_parse/xml_parse.hpp
+++ xml_parse/xml_parse.hpp
@@ -3,3 +3,6 @@
 #include "sxml_parse_string.hpp"
 #include "sxml_parse_fundamental.hpp"
 #include "sxml_parse_iterable.hpp"
+#include "sxml_parse_fusion.hpp"
+#include "sxml_parse_object.hpp"
+#include "sxml_parse_vector.hpp"
