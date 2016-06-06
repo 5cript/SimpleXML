4b2de9656d9b0663896167c7536aacd989a03334
5cript <Tim06TR@gmail.com>
2016-06-06 20:30:53 +0200

    Added unoredered map parser.

---
+++ xml_parse/sxml_parse_unordered_map.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "sxml_parse_core.hpp"
+
+#include <unordered_map>
+
+namespace SXML
+{
+    template <typename ValueT, typename HashT = std::hash <std::string>, class PredT = std::equal_to <std::string>,
+              typename AllocT = std::allocator <std::pair <const std::string, ValueT>>>
+    void xml_parse(std::unordered_map<std::string, ValueT, HashT, PredT, AllocT>& value, NodeName const& name,
+               PropertyTree const& object, XmlParseOptions const& options = {})
+    {
+        try
+        {
+            auto TEST = static_cast <std::string> (name);
+            GET_CHILD(name, pt, (std::unordered_map <std::string, ValueT>()));
+            for (auto const& i : pt)
+            {
+                ValueT temp;
+                xml_parse(temp, name >> i.first, object, options);
+                value[i.first] = temp;
+            }
+        }
+        DEFAULT_CATCH({}, {})
+    }
+}
