578114c8afdcda332062b1e327a0787b1b3a57db
5cript <tim06tr@gmail.com>
2019-08-26 20:27:48 +0200

    Added support for std::optional.

--- xml_parse/sxml_parse_optional.hpp
+++ xml_parse/sxml_parse_optional.hpp
@@ -4,6 +4,12 @@
 
 #include <boost/optional.hpp>
 
+#ifdef __has_include                           // Check if __has_include is present
+#if __has_include(<optional>)                // Check for a standard library
+#include <optional>
+#endif
+#endif // defined(__has_include)
+
 namespace SXML
 {
     template <typename T>
@@ -23,4 +29,26 @@ namespace SXML
         }
         SXML_DEFAULT_CATCH(boost::none, boost::none);
     }
+
+#ifdef __has_include                           // Check if __has_include is present
+#if __has_include(<optional>)
+    template <typename T>
+    void xml_parse(std::optional <T>& value, NodeName const& name, PropertyTree const& object, XmlParseOptions const& options)
+    {
+        try
+        {
+            if (!options.stateMixing)
+                value = std::nullopt;
+
+            if (object.tree.get_optional <std::string>(static_cast <std::string>(name)))
+            {
+                T v;
+                xml_parse(v, name, object, options);
+                value = v;
+            }
+        }
+        SXML_DEFAULT_CATCH(std::nullopt, std::nullopt);
+    }
+#endif
+#endif // __has_include
 }
--- xmlify/sxml_optional.hpp
+++ xmlify/sxml_optional.hpp
@@ -4,6 +4,12 @@
 
 #include <boost/optional.hpp>
 
+#ifdef __has_include                           // Check if __has_include is present
+#if __has_include(<optional>)                // Check for a standard library
+#include <optional>
+#endif
+#endif // defined(__has_include)
+
 namespace SXML
 {
     template <typename T>
@@ -13,4 +19,16 @@ namespace SXML
             return xmlify(stream, name, value.get(), options);
         return stream;
     }
+
+#ifdef __has_include                           // Check if __has_include is present
+#if __has_include(<optional>)
+    template <typename T>
+    std::ostream& xmlify(std::ostream& stream, std::string const& name, std::optional <T> const& value, XmlifyOptions const& options = {})
+    {
+        if (value)
+            return xmlify(stream, name, value.value(), options);
+        return stream;
+    }
+#endif
+#endif // __has_include
 }
